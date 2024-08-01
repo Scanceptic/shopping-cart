330804eb356e640afddb0fedbf0ee4e6f4ecbf7f
Nathan <urwin.nathan@gmail.com>
2024-08-01 16:52:16 +0100

    remove old empty css file

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import Shopping from './Shopping.jsx';
-import './index.css';
 
 const router = createBrowserRouter([
   {
